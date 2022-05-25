dcc42dc6097fcc2658305fc6391d529a4a8755ba
Vinicius <viniciusflexa@gmail.com>
2022-05-25 16:25:38 -0300

    first start profile creation

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -6,7 +6,19 @@ export function Profile(){
             <Header />
 
             <div>
-                
+                <div className="bg-zinc-300 shadow-inner shadow-zinc-800 w-full h-28"></div>
+                <div>
+                    <div className="flex flex-col w-full h-full ml-6">
+                        <div className="rounded-full bg-red-600 w-36 h-36">
+                            <img src="" alt="" />
+                        </div>
+
+                        <div className="flex flex-col">
+                            <span className="text-lg font-bold">Teste</span>
+                            <span className="text-sm">@teste</span>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     )
