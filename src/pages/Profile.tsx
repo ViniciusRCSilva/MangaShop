e2ded07401cc2ed0246e9a3529c78d0e8f412a56
Vinicius <viniciusflexa@gmail.com>
2022-05-28 16:44:11 -0300

    removing position fixed

--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -26,88 +26,86 @@ export function Profile({ name, nick, address, number }: ProfileContentProps) {
         <div>
             <Header />
 
-            <div className="fixed w-full h-full">
-                <div className="flex flex-col items-start justify-center md:items-center mt-10 w-full h-[70vh]">
+            <div className="flex flex-col items-start justify-center md:items-center mt-10 w-full h-[70vh]">
 
-                    <div className="flex flex-row justify-between w-[90%] ml-6 md:w-[50%] md:ml-0">
+                <div className="flex flex-row justify-between w-[90%] ml-6 md:w-[50%] md:ml-0">
 
-                        <div className="flex flex-row">
-                            <div className="rounded-full bg-red-600 w-36 h-36">
-                                <img src="" alt="" />
-                            </div>
-
-                            <div className="flex flex-col ml-6">
-                                <span className="text-xl font-bold">{name}</span>
-                                <span className="text-sm">@{nick}</span>
-                            </div>
+                    <div className="flex flex-row">
+                        <div className="rounded-full bg-red-600 w-36 h-36">
+                            <img src="" alt="" />
                         </div>
 
-                        <Link to='/perfil'>
-                            <div className="flex flex-row h-7 link link-underline link-underline-black">
-                                <Gear className="text-2xl mr-2" />
-                                <div className="hidden md:flex">
-                                    <span>Editar</span>
-                                </div>
-                            </div>
-                        </Link>
-
+                        <div className="flex flex-col ml-6">
+                            <span className="text-xl font-bold">{name}</span>
+                            <span className="text-sm">@{nick}</span>
+                        </div>
                     </div>
 
-                    <div className="flex flex-col w-full md:w-[50%] h-full rounded-xl p-6">
-
-                        <div className="flex flex-col mt-3">
-                            <div className="flex flex-row items-center justify-between w-full">
-
-                                <div onClick={() => handleChangeView("info")} className="flex flex-col items-center hover:text-red-600 cursor-pointer transition-colors">
-                                    <UserList className="text-4xl" />
-                                    <span className="hidden md:flex">Informações</span>
-                                </div>
-
-                                <div onClick={() => handleChangeView("favorites")} className="flex flex-col items-center hover:text-red-600 cursor-pointer transition-colors">
-                                    <Heart className="text-4xl" />
-                                    <span className="hidden md:flex">Favoritos</span>
-                                </div>
-
-                                <div onClick={() => handleChangeView("views")} className="flex flex-col items-center hover:text-red-600 cursor-pointer transition-colors">
-                                    <ClockCounterClockwise className="text-4xl" />
-                                    <span className="hidden md:flex">Visualizados</span>
-                                </div>
-
-                                <div onClick={() => handleChangeView("coupons")} className="flex flex-col items-center hover:text-red-600 cursor-pointer transition-colors">
-                                    <Tag className="text-4xl" />
-                                    <span className="hidden md:flex">Cupons</span>
-                                </div>
-
-                                <div onClick={() => handleChangeView("ordered")} className="flex flex-col items-center hover:text-red-600 cursor-pointer transition-colors">
-                                    <ShoppingBagOpen className="text-4xl" />
-                                    <span className="hidden md:flex">Pedidos</span>
-                                </div>
-
+                    <Link to='/perfil'>
+                        <div className="flex flex-row h-7 link link-underline link-underline-black">
+                            <Gear className="text-2xl mr-2" />
+                            <div className="hidden md:flex">
+                                <span>Editar</span>
                             </div>
                         </div>
+                    </Link>
 
-                        <div className="flex flex-col border-2 mt-3 rounded-lg h-full p-4">
-                            {contentView === 'info' && (
-                                <InfoProfile address={address} number={number} />
-                            )}
-                            {contentView === 'favorites' && (
-                                <FavProfile />
-                            )}
-                            {contentView === 'views' && (
-                                <ViewsProfile />
-                            )}
-                            {contentView === 'coupons' && (
-                                <CouponProfile />
-                            )}
-                            {contentView === 'ordered' && (
-                                <OrderedProfile />
-                            )}
-                        </div>
-
-                    </div>
                 </div>
 
+                <div className="flex flex-col w-full md:w-[50%] h-full rounded-xl p-6">
+
+                    <div className="flex flex-col mt-3">
+                        <div className="flex flex-row items-center justify-between w-full">
+
+                            <div onClick={() => handleChangeView("info")} className="flex flex-col items-center hover:text-red-600 cursor-pointer transition-colors">
+                                <UserList className="text-4xl" />
+                                <span className="hidden md:flex">Informações</span>
+                            </div>
+
+                            <div onClick={() => handleChangeView("favorites")} className="flex flex-col items-center hover:text-red-600 cursor-pointer transition-colors">
+                                <Heart className="text-4xl" />
+                                <span className="hidden md:flex">Favoritos</span>
+                            </div>
+
+                            <div onClick={() => handleChangeView("views")} className="flex flex-col items-center hover:text-red-600 cursor-pointer transition-colors">
+                                <ClockCounterClockwise className="text-4xl" />
+                                <span className="hidden md:flex">Visualizados</span>
+                            </div>
+
+                            <div onClick={() => handleChangeView("coupons")} className="flex flex-col items-center hover:text-red-600 cursor-pointer transition-colors">
+                                <Tag className="text-4xl" />
+                                <span className="hidden md:flex">Cupons</span>
+                            </div>
+
+                            <div onClick={() => handleChangeView("ordered")} className="flex flex-col items-center hover:text-red-600 cursor-pointer transition-colors">
+                                <ShoppingBagOpen className="text-4xl" />
+                                <span className="hidden md:flex">Pedidos</span>
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="flex flex-col border-2 mt-3 rounded-lg h-full p-4">
+                        {contentView === 'info' && (
+                            <InfoProfile address={address} number={number} />
+                        )}
+                        {contentView === 'favorites' && (
+                            <FavProfile />
+                        )}
+                        {contentView === 'views' && (
+                            <ViewsProfile />
+                        )}
+                        {contentView === 'coupons' && (
+                            <CouponProfile />
+                        )}
+                        {contentView === 'ordered' && (
+                            <OrderedProfile />
+                        )}
+                    </div>
+
+                </div>
             </div>
-            </div>
+
+        </div>
     )
 }
