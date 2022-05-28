4a9f74e4400046d3381d8fb67eaf30692693d092
Vinicius <viniciusflexa@gmail.com>
2022-05-28 16:35:58 -0300

    changing style manga window

--- src/components/MangaWindow.tsx
+++ src/components/MangaWindow.tsx
@@ -5,14 +5,14 @@ interface MangaWindowProps{
     tag?: String
 }
 
-export function MangaWindow(props: MangaWindowProps){
+export function MangaWindow({ image, title, desc, tag }: MangaWindowProps){
     return(
-        <div className="flex flex-col w-[200px] border-2 md:mb-0 rounded-lg cursor-pointer hover:border-red-600 transition-all md:hover:scale-105">
-            <div style={{backgroundImage: `url(${props.image})`}} className="w-[196px] h-20 bg-[url(backgroundImage)] rounded-t-md bg-center bg-cover"></div>
-            <span className="text-lg pl-2 font-bold">{props.title}</span>
-            <span className="text-xs pl-3 pr-3 pb-2 pt-2 text-justify">{props.desc}</span>
+        <div className="flex flex-col w-[200px] border-2 rounded-lg cursor-pointer hover:border-red-600 transition-all md:hover:scale-105">
+            <div style={{backgroundImage: `url(${image})`}} className="w-[196px] h-20 bg-[url(backgroundImage)] rounded-t-md bg-center bg-cover"></div>
+            <span className="text-lg pl-2 font-bold">{title}</span>
+            <span className="text-xs pl-3 pr-3 pb-2 pt-2 text-justify">{desc}</span>
             <div className="bg-red-600 h-1"></div>
-            <span className="text-xs p-2">{props.tag}</span>
+            <span className="text-xs p-2">{tag}</span>
         </div>
     )
 }
