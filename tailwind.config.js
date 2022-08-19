4edd4ae471124699772efb186eb4558dbe5b4b13
Vinicius <viniciusflexa@gmail.com>
2022-08-19 11:02:14 -0300

    colorChange animation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,20 @@
 module.exports = {
   darkMode: ['class', '[data-mode="dark"]'],
   content: ["./src/**/*.tsx"],
-  theme: {},
+  theme: {
+    extend: {
+      animation: {
+        colorChange: 'colorChange 1s infinite',
+      },
+      keyframes: {
+        colorChange: {
+          '0%': { color: '#000' },
+          '50%': { color: '#ff2626' },
+          '100%': { color: '#000' },
+        }
+      }
+    }
+  },
   plugins: [
     require('@tailwindcss/forms'),
     require('tailwind-scrollbar'),
