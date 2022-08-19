75bdfad988e7265d77772dcf8f64a50b845d9851
Vinicius <viniciusflexa@gmail.com>
2022-08-19 11:03:31 -0300

    font and style changed

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { Cards, CaretDown, Gear, House, List, MagnifyingGlass, Power, ShoppingCart, UserCircle } from "phosphor-react";
+import { BookOpen, Cards, CaretDown, Gear, House, List, MagnifyingGlass, Power, ShoppingCart, UserCircle } from "phosphor-react";
 import { LinkTo } from "./LinksForHeader/LinkTo";
 import { useState } from "react";
 
@@ -22,8 +22,9 @@ export function Header(){
     return(
         <div className="fixed w-full bg-white dark:bg-slate-800 dark:text-zinc-50">
             <div className="flex flex-row items-center justify-between pl-6 pr-6 pt-6 pb-6 shadow-lg">
-                <div className="text-xl font-bold font-['Kaushan_Script'] bg-red-600 hover:bg-red-500 transition-colors text-zinc-50 p-3 rounded-xl">
-                    <LinkTo linkPath={'/'} linkName={'MANGÁ SHOP'} />
+                <div className="flex flex-row items-center text-xl font-bold font-['Raleway'] text-red-600 hover:text-red-500 transition-colors">
+                    <LinkTo linkPath={'/'} linkName={'mangáSHOP'} />
+                    <BookOpen className="ml-1 italic"/>
                 </div>
 
                 <div className="hidden md:flex flex-row">
--- src/components/HeaderForPresentation/HeaderPresentation.tsx
+++ src/components/HeaderForPresentation/HeaderPresentation.tsx
@@ -1,10 +1,12 @@
+import { BookOpen } from "phosphor-react";
 import { LinkTo } from "../LinksForHeader/LinkTo";
 
 export function HeaderPresentation(){
     return(
         <div className="flex flex-row items-center justify-between pl-6 pr-6 pt-6 pb-6 shadow-lg">
-            <div className="text-xl font-bold font-['Kaushan_Script'] bg-red-600 hover:bg-red-500 transition-colors text-zinc-50 p-3 rounded-xl">
-                <LinkTo linkPath={'/apresentacao'} linkName={'MANGÁ SHOP'} />
+            <div className="flex flex-row items-center text-xl font-bold font-['Raleway'] text-red-600 hover:text-red-500 transition-colors">
+                <LinkTo linkPath={'/'} linkName={'mangáSHOP'} />
+                <BookOpen className="ml-1 italic"/>
             </div>
         </div>
     )
